37f2372e50c5d247c360822bc81e2c644aed56bb
James Harrington <jharrington@victorytailgate.com>
2019-01-18 01:32:50 -0500

    Cleaning up create password page html

--- src/pages/Intro/CreatePassword.vue
+++ src/pages/Intro/CreatePassword.vue
@@ -1,41 +1,37 @@
 <template>
 <q-page class="flex flex-center text-white bg-main">
-  <div class="row flex-center text-left">
-    <div class="col-12">
-      <div class="text-weight-bold q-pa-lg">
-        <big class="titillium">{{ $t('CreateVertoPassword.header') }}</big>
+  <q-card style="max-width: 700px;" flat>
+    <q-card-title class="text-weight-bold">
+      <big class="titillium">{{ $t('CreateVertoPassword.header') }}</big>
+    </q-card-title>
+    <q-card-main class="text-left">
+      <div class="text-left q-py-lg">
+        <span class="display-inline text-yellow">
+          <q-icon name="error" size="1.2rem" color="yellow" class="q-mr-xs q-mb-sm"/>
+          <span class="q-title">
+            {{ $t('CreateVertoPassword.important') }}:
+          </span>
+        </span>
+        <span class="q-subheading">
+          {{ $t('CreateVertoPassword.important_warning') }}
+        </span>
       </div>
-      <q-card class="" style="max-width: 700px;">
-        <q-card-main class="text-left">
-          <div class="text-left q-pa-lg">
-            <span class="display-inline text-yellow">
-              <q-icon name="error" size="1.2rem" color="yellow" class="q-mr-xs q-mb-sm"/>
-              <span class="q-title">
-                {{ $t('CreateVertoPassword.important') }}:
-              </span>
-            </span>
-            <span class="q-subheading">
-              {{ $t('CreateVertoPassword.important_warning') }}
-            </span>
-          </div>
-          <div class="text-left q-pa-lg text-white">
-            <q-field :error="passHasError" error-label="Password Cannot Be Null" :count="100" >
-              <q-input type="password" dark v-model="password" color="yellow" float-label="Verto Password"/>
-            </q-field>
-            <q-field :error="doesNotMatch" error-label="Passwords do not match" :count="100" >
-              <q-input type="password" dark v-model="confirmPassword" color="yellow" float-label="Confirm Verto Password"/>
-            </q-field>
-          </div>
-          <div class="text-left q-pa-lg">
-            <q-checkbox v-model="checked" color="yellow">&nbsp;{{ $t('CreateVertoPassword.i_understand') }}</q-checkbox>
-          </div>
-          <div class="text-right q-pa-lg">
-            <q-btn color="yellow" outline rounded @click="submit">Submit</q-btn>
-          </div>
-        </q-card-main>
-      </q-card>
-    </div>
-  </div>
+      <div class="text-left q-py-lg text-white">
+        <q-field :error="passHasError" error-label="Password Cannot Be Null" :count="100" >
+          <q-input type="password" dark v-model="password" color="yellow" float-label="Verto Password"/>
+        </q-field>
+        <q-field :error="doesNotMatch" error-label="Passwords do not match" :count="100" >
+          <q-input type="password" dark v-model="confirmPassword" color="yellow" float-label="Confirm Verto Password"/>
+        </q-field>
+      </div>
+      <div class="text-left q-py-lg">
+        <q-checkbox v-model="checked" color="yellow">&nbsp;{{ $t('CreateVertoPassword.i_understand') }}</q-checkbox>
+      </div>
+      <div class="text-right q-py-lg">
+        <q-btn color="yellow" outline rounded @click="submit">Submit</q-btn>
+      </div>
+    </q-card-main>
+  </q-card>
 </q-page>
 </template>
 
