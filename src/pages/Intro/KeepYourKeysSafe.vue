d3699f2f7c2d67159323365f089f4f49eabc1d8b
James Harrington <jharrington@victorytailgate.com>
2019-01-18 01:33:10 -0500

    applying password page styles to keep your key safe

--- src/pages/Intro/KeepYourKeysSafe.vue
+++ src/pages/Intro/KeepYourKeysSafe.vue
@@ -1,65 +1,52 @@
 <template>
-  <q-page class="flex fit flex-center q-pa-sm bg-main">
-    <q-jumbotron class="text-center bg-primary text-white">
-      <div class="text-weight-bold uppercase q-pa-lg">
-        <big>{{ $t('KeepYourKeysSafe.keepsafe') }}</big>
+<q-page class="flex flex-center text-white bg-main">
+  <q-card style="max-width: 700px;" flat>
+    <q-card-title class="text-weight-bold">
+      <big class="titillium">{{ $t('KeepYourKeysSafe.keepsafe') }}</big>
+    </q-card-title>
+    <q-card-main class="text-left">
+      <div class="q-py-sm">
+        {{ $t('KeepYourKeysSafe.before') }}
       </div>
-      <q-card inline style="width: 700px" class="text-left">
-        <q-card-separator />
-
-          <q-list>
-            <div class="q-pa-sm">
-                {{ $t('KeepYourKeysSafe.before') }}
-              </div>
-            <q-collapsible icon="explore" label="Know More">
-
-              <div class="q-pa-sm">
-                {{ $t('KeepYourKeysSafe.choices') }}
-              </div>
-              <div class="q-pa-sm">
-                {{ $t('KeepYourKeysSafe.first') }}
-              </div>
-              <div class="q-pa-sm">
-                {{ $t('KeepYourKeysSafe.second') }}
-              </div>
-              <div class="q-pa-sm">
-                {{ $t('SaveYourKeys.strategy') }}
-              </div>
-              <div class="q-pa-sm">
-                <p v-html="$t('SaveYourKeys.physical')"></p>
-              </div>
-              <div class="q-pa-sm">
-                <p v-html="$t('SaveYourKeys.legacy')"></p>
-              </div>
-            </q-collapsible>
-          </q-list>
-          <div class="q-pa-sm">
-            <div class="q-pa-sm">
-              <q-checkbox v-model="checkOne" native-value="first" color="yellow">&nbsp;{{ $t('SaveYourKeys.agree') }}</q-checkbox>
-              <q-checkbox v-model="checkTwo" native-value="second" color="yellow">&nbsp;{{ $t('SaveYourKeys.responsible') }}</q-checkbox>
-              <q-checkbox v-model="checkThree" native-value="second" color="yellow">&nbsp;{{ $t('SaveYourKeys.access') }}</q-checkbox>
-              <q-checkbox v-model="checkFour" native-value="second" color="yellow">&nbsp;{{ $t('SaveYourKeys.wifi') }}</q-checkbox>
-            </div>
-          </div>
-          <div class="q-pa-sm">
-            <div class="q-pa-sm">
-              <q-select
-                dark
-                color="white"
-                separator
-                v-model="select"
-                :options="options"
-              />
-            </div>
-
-            <div class="text-right q-pa-lg">
-              <q-btn color="yellow" outline rounded @click="submit">{{ $t('SaveYourKeys.create') }}</q-btn>
-            </div>
-          </div>
-      </q-card>
-
-    </q-jumbotron>
-  </q-page>
+      <q-collapsible icon="explore" label="Learn More">
+        <div class="q-py-sm">
+          {{ $t('KeepYourKeysSafe.choices') }}
+        </div>
+        <div class="q-py-sm">
+          {{ $t('KeepYourKeysSafe.first') }}
+        </div>
+        <div class="q-py-sm">
+          {{ $t('KeepYourKeysSafe.second') }}
+        </div>
+        <div class="q-py-sm">
+          {{ $t('SaveYourKeys.strategy') }}
+        </div>
+        <div class="q-py-sm">
+          <p v-html="$t('SaveYourKeys.physical')"></p>
+        </div>
+        <div class="q-py-sm">
+          <p v-html="$t('SaveYourKeys.legacy')"></p>
+        </div>
+      </q-collapsible>
+      <div class="q-py-sm">
+        <div class="q-py-sm">
+          <q-checkbox v-model="checkOne" native-value="first" color="yellow">&nbsp;{{ $t('SaveYourKeys.agree') }}</q-checkbox>
+          <q-checkbox v-model="checkTwo" native-value="second" color="yellow">&nbsp;{{ $t('SaveYourKeys.responsible') }}</q-checkbox>
+          <q-checkbox v-model="checkThree" native-value="second" color="yellow">&nbsp;{{ $t('SaveYourKeys.access') }}</q-checkbox>
+          <q-checkbox v-model="checkFour" native-value="second" color="yellow">&nbsp;{{ $t('SaveYourKeys.wifi') }}</q-checkbox>
+        </div>
+      </div>
+      <div class="q-pa-sm">
+        <div class="q-py-sm">
+          <q-select dark color="white" separator v-model="select" :options="options" />
+        </div>
+        <div class="text-right q-pa-lg">
+          <q-btn color="yellow" outline rounded @click="submit">{{ $t('SaveYourKeys.create') }}</q-btn>
+        </div>
+      </div>
+    </q-card-main>
+  </q-card>
+</q-page>
 </template>
 
 <script>
