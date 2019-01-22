a8f65824a20828168b5eb868b25652a0755b8e0d
Gregory Luneau <gregory.luneau@gmail.com>
2019-01-21 22:20:04 -0500

    French and English are enabled

--- src/components/layout/LangDrawer.vue
+++ src/components/layout/LangDrawer.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="fit text-white text-center bg-primary">
     <div class="q-pa-xl">
-      <q-icon name="account_balance_wallet" size="2rem"/>
-      <div class="q-headline">English</div>
+      <a class="" @click="updateLanguage('en')">
+        <q-icon name="account_balance_wallet" size="2rem"/>
+        <div class="q-headline">English</div>
+      </a>
     </div>
     <div class="q-pa-xl">
-      <q-icon name="account_balance_wallet" size="2rem"/>
-      <div class="q-headline">French</div>
+      <a class="" @click="updateLanguage('fr')">
+        <q-icon name="account_balance_wallet" size="2rem"/>
+        <div class="q-headline">French</div>
+      </a>
     </div>
   </div>
 </template>
@@ -15,7 +19,18 @@
 export default {
   name: 'LangDrawer',
   data () {
-    return {}
+    return {
+      open: false,
+      langs: ['en', 'fr', 'ru', 'es', 'el', 'ar', 'tr', 'pl', 'zh', 'nl', 'az', 'pt', 'sr', 'fil'],
+      showParents: true,
+      appVersion: this.$appVersion,
+      appName: this.$appName
+    }
+  },
+  methods: {
+    updateLanguage: function (lang) {
+      this.$i18n.locale = lang
+    }
   }
 }
 </script>
